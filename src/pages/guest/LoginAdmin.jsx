9b95350e4cf4dc6f415f78f7e8a2202fc184481c
lina marin j <linamarin2011@gmail.com>
2025-05-01 13:21:05 -0500

    adjustment in styles

--- src/pages/guest/LoginAdmin.jsx
+++ src/pages/guest/LoginAdmin.jsx
@@ -1,13 +1,10 @@
-
-import './LoginAdmin.css';
-import { usuarios } from '../../services/database';
-import { useState } from 'react';
+import "./LoginAdmin.css";
+import { usuarios } from "../../services/database";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { alerta, generarToken} from '../../helpers/funciones';
+import { alerta, generarToken } from "../../helpers/funciones";
 
 function LoginAdmin() {
-
-
   const [getUser, setUser] = useState("");
   const [getPassword, setPassword] = useState("");
   let redireccion = useNavigate();
@@ -21,8 +18,8 @@ function LoginAdmin() {
 
   function iniciarSesion() {
     if (buscarUsuario()) {
-      let tokenAcceso = generarToken()
-      localStorage.setItem("token", tokenAcceso) 
+      let tokenAcceso = generarToken();
+      localStorage.setItem("token", tokenAcceso);
       alerta("Bienvenido", "Acceso al sistema", "success");
       redireccion("/admin");
     } else {
@@ -30,23 +27,34 @@ function LoginAdmin() {
     }
   }
 
-
-
   return (
     <div className="container">
       <div className="left-section">
         <h1>Quick Wash</h1>
         <p>El Mejor Servicio De AutoLavado Para Tu Vehiculo</p>
-        <button className="read-more">Cotiza o Pregunta Por Nuestros Servicios Aqui</button>
       </div>
 
       <div className="right-section">
         <h2>Hola De nuevo!</h2>
         <p>Ingresa tus datos</p>
         <form id="loginForm">
-          <input type="email" id="email" placeholder="Correo"  onChange={(e) => setUser(e.target.value)} required />
-          <input type="password" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
-          <button type="button" onClick={iniciarSesion}>Login</button>
+          <input
+            type="email"
+            id="email"
+            placeholder="Correo"
+            onChange={(e) => setUser(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="button" onClick={iniciarSesion}>
+            Login
+          </button>
         </form>
         <p className="forgot-password">¿Olvidaste tu contrasena?</p>
       </div>
--- src/pages/guest/OurServices.jsx
+++ src/pages/guest/OurServices.jsx
@@ -15,7 +15,7 @@ function OurServices() {
             <h3 className="mt-4 text-lg font-medium text-white">
               Lavado externo
             </h3>
-            <p className="mt-1 text-lg text-white">$60.000</p>
+            <p className="mt-1 text-lg text-white">$50.000</p>
           </a>
           <a href="#" className="group">
             <img
@@ -44,7 +44,7 @@ function OurServices() {
               className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
             />
             <h3 className="mt-4 text-lg font-medium text-white">Polichado</h3>
-            <p className="mt-1 text-lg text-white">$160.000</p>
+            <p className="mt-1 text-lg text-white">$150.000</p>
           </a>
         </div>
       </div>
